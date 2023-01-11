c60fcb42fb1497fb9a2823ad81cdfce0335a4a1d
Antoine ZUNDEL <92809568+toinez2000@users.noreply.github.com>
2023-01-11 21:27:13 +0100

    Update convolutionLayer.cu

--- convolutionLayer.cu
+++ convolutionLayer.cu
@@ -10,21 +10,21 @@
 
 __global__ void addLayerOutput(float* input, float* output, int SxI, int SzI, int SxK, int SzK) {
   // Calculer les indices de l'élément de sortie courant
-  int xo = blockIdx.x * blockDim.x; // ligne de l'image de sortie pour une profondeur donné 
-  int yo = threadIdx.x; // element de la ligne
-  int zK = blockIdx.z ;
-  int Sxo = (SxI-SxK+1);
+  int xo = blockIdx.x * blockDim.x; // ligne de l'image de sortie pour une Deep input et output donnée
+  int yo = threadIdx.x; // element de la ligne pour une Deep input et output donnée
+  int zK = blockIdx.z ; // Deep du kernel  
+  int Sxo = (SxI-SxK+1); // Size output en x
 
 	float value =0;
   
 		// Appliquer le filtre sur chaque élément de l'image
 	  for (int i = 0; i < SzI; i++) {
-          	// Appliquer le filtre à l'élément de l'image courant
-           value += input[xo+yo+  zK*Sxo*Sxo + i*SzK*Sxo*Sxo];
+          	
+           value += input[xo+yo+  zK*Sxo*Sxo + i*SzK*Sxo*Sxo];  // sum(Cx_data[x_c= const][y_c = const ][zK = const][zI]) 
 	      
 	    }
 
-  	  // Enregistrer la valeur de l'élément de sortie
+  	  // Enregistrer la valeur dans la sortie
     output[xo+ yo + zK*Sxo*Sxo] = value;
          
 }
@@ -34,15 +34,15 @@ __global__ void addLayerOutput(float* input, float* output, int SxI, int SzI, in
 
 __global__ void conv3D(float* input, float* kernel, float* output, int SxI, int SzI, int SxK, int SzK) {
   // Calculer les indices de l'élément de sortie courant
-  int xo = blockIdx.x * blockDim.x; // ligne de l'image de sortie pour une profondeur donné 
+  int xo = blockIdx.x * blockDim.x; // ligne de l'image de sortie pour une profondeur donnée 
   int yo = threadIdx.x; // element de la ligne
-  int Sxo = (SxI-SxK+1);
-  int Szo = SzK;
+  int Sxo = (SxI-SxK+1); //Size output x
+  int Szo = SzK; //Deep output 
 
  
 
   int zK = blockIdx.z ;   //profondeur du kernel à utiliser 
-  int zI = threadIdx.z; // proffondeur de l'image input à utilisé 
+  int zI = threadIdx.z; // proffondeur de l'image input à utiliser
 
   // Initialiser la valeur de l'élément de sortie à 0
   float value = 0.0;
@@ -68,21 +68,15 @@ __global__ void conv3D(float* input, float* kernel, float* output, int SxI, int 
 	      }
 	    }
 
-  	  // Enregistrer la valeur de l'élément de sortie
-    output[xo+ yo + zK*Sxo*Sxo + zI*Szo*Sxo*Sxo] = value;
-	//output[7] = value;
-
-	//if(xo ==0 && yo == 0 && zK ==1){printf("output = %f \n",output[zK*Sxo*Sxo]);}
-	
-  
-         
+  	  // Enregistrer la valeur
+    output[xo+ yo + zK*Sxo*Sxo + zI*Szo*Sxo*Sxo] = value;     
 }
 
 
 __global__ void addBias(float* M, float*bias, int SxI, int SzI) {
   // Calculer les indices de l'élément de sortie courant
-  int deep = blockIdx.x; //  
-  int yo = threadIdx.x; // 
+  int deep = blockIdx.x; //  profondeur
+  int yo = threadIdx.x; // element 
  
 
      //printf("bias = %f ", bias[deep]);
@@ -99,23 +93,22 @@ float* vectorGPUConv1 (float* Kernel, float* input,int SxI,int SzI,int SxK,int S
 {
 
 	float *out ;
-	float *d_input, *d_Kernel, *d_out, *d_out0;
-  int Sxo = SxI-SxK+1;
-  int Szo = SzK;
-    // printf("bias = %f ", Kernel[SzI*SxK*SxK*SzK+1]);
+	float *d_input, *d_Kernel, *d_out, *d_out0;  
+  	int Sxo = SxI-SxK+1;  //size output x convolution layer
+  	int Szo = SzK;
+
 
 	out = (float*)malloc(sizeof(float) * Sxo*Sxo*Szo);
 /*
 	printf("C1 \n\n");
     print_matrix(out,Sxo*Szo*SzI,Sxo);
-
 */
 
-	cudaMalloc((void**)&d_input, sizeof(float)*SxI*SxI*SzI);
+	//réservation de mémoire CPU
+	cudaMalloc((void**)&d_input, sizeof(float)*SxI*SxI*SzI);  
     	cudaMalloc((void**)&d_Kernel, sizeof(float)*(SxK*SxK*SzK*SzI+SzK));
     	cudaMalloc((void**)&d_out, sizeof(float)*Sxo*Sxo*Szo);
-
-		cudaMalloc((void**)&d_out0, sizeof(float)*Sxo*Sxo*Szo*SzI);
+	cudaMalloc((void**)&d_out0, sizeof(float)*Sxo*Sxo*Szo*SzI);  //d_out0 output intermédiaire cette matrice contient l'output de la convolution Sxo*Sxo*Szo*SzI
 
 	
     	cudaMemcpy(d_Kernel, Kernel, sizeof(float) *(SxK*SxK*SzK*SzI+SzK), cudaMemcpyHostToDevice);
@@ -130,7 +123,7 @@ float* vectorGPUConv1 (float* Kernel, float* input,int SxI,int SzI,int SxK,int S
  
       
 
-    	conv3D<<<threadsPerBlock,blocks>>>(d_input, d_Kernel, d_out0, SxI, SzI, SxK,  SzK);   //SIZE_C1_kernel
+    	conv3D<<<threadsPerBlock,blocks>>>(d_input, d_Kernel, d_out0, SxI, SzI, SxK,  SzK);  
 		cudaDeviceSynchronize();
 		dim3 blocks1( Sxo, 1, 1); 
 
@@ -142,11 +135,11 @@ float* vectorGPUConv1 (float* Kernel, float* input,int SxI,int SzI,int SxK,int S
 		cudaDeviceSynchronize();
     	cudaMemcpy(out, d_out, sizeof(float)*Sxo*Sxo*Szo, cudaMemcpyDeviceToHost);
 	
-	
+	//free
     	cudaFree(d_Kernel);
     	cudaFree(d_input);
     	cudaFree(d_out);
-		cudaFree(d_out0);
+	cudaFree(d_out0);
 
 	return out;
 
