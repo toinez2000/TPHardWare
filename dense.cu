286156250d3782080ad62afbd05450e4f4657bba
Antoine ZUNDEL <92809568+toinez2000@users.noreply.github.com>
2023-01-11 21:47:06 +0100

    Update dense.cu

--- dense.cu
+++ dense.cu
@@ -6,7 +6,7 @@
 #include "functionMatrix.h"
 #include "dense.h"
 
-
+//fonction add bias 
 __global__ void addBias(float *input,float *bias){
     
   int ip = threadIdx.x;
@@ -15,16 +15,18 @@ __global__ void addBias(float *input,float *bias){
 
 }
 
+
+
 __global__ void Dense(float *input,float *weight,float *output,int SxI,int Sxo){
     
-  int tabIP = blockIdx.x ;
+  int tabIP = blockIdx.x ; //for each output you add multiplication of weight and input
 
   for(int ip =0;ip < SxI;ip++){
     output[tabIP] += input[ip]*weight[tabIP*SxI+ip];}
 
 }
 
-
+// active function TanH
 __global__ void DTanH(float *input,int SxI){
     
   
@@ -34,6 +36,7 @@ __global__ void DTanH(float *input,int SxI){
 
 }
 
+//active function softMax
 __global__ void softMax(float *input,int SxI,float sum)
 {
   int ip = threadIdx.x;
@@ -41,7 +44,7 @@ __global__ void softMax(float *input,int SxI,float sum)
   input[ip] = input[ip]/sum;
 }
 
-
+//function calcul the exp of each output
 __global__ void Expo(float *input,int SxI)
 {
   int ip = threadIdx.x;
@@ -69,14 +72,14 @@ output0= (float*)malloc(sizeof(float) *Sxo);
 
     
 
-
+//malloc GPU
 	cudaMalloc((void**)&d_input, sizeof(float)*SxI);
     	cudaMalloc((void**)&d_Weight, sizeof(float)*(SxI*Sxo+Sxo));
     	cudaMalloc((void**)&d_out, sizeof(float)*Sxo);
 	
     	cudaMemcpy(d_Weight, Weight, sizeof(float) *(SxI*Sxo+Sxo), cudaMemcpyHostToDevice);
     	cudaMemcpy(d_input, input, sizeof(float) *SxI, cudaMemcpyHostToDevice);
-      cudaMemcpy(d_out,output,sizeof(float) *Sxo,cudaMemcpyHostToDevice);
+      	cudaMemcpy(d_out,output,sizeof(float) *Sxo,cudaMemcpyHostToDevice);
 
 	// Main function
 
@@ -84,11 +87,11 @@ output0= (float*)malloc(sizeof(float) *Sxo);
  
       
 
-    	 Dense<<<blocks,1>>>(d_input, d_Weight, d_out, SxI,  Sxo);   //SIZE_C1_kernel
+    	 Dense<<<blocks,1>>>(d_input, d_Weight, d_out, SxI,  Sxo);   //calcul each output of dense without bias
        cudaDeviceSynchronize();
- 	    addBias<<<Sxo,1>>>(d_out,d_Weight+SxI*Sxo);
+ 	    addBias<<<Sxo,1>>>(d_out,d_Weight+SxI*Sxo); //add bias
       cudaDeviceSynchronize();
-      if(ActiveFunction==0){DTanH<<<1,Sxo>>>(d_out,Sxo);
+      if(ActiveFunction==0){DTanH<<<1,Sxo>>>(d_out,Sxo); 
       cudaDeviceSynchronize();
       
       } //TanH
@@ -107,7 +110,7 @@ output0= (float*)malloc(sizeof(float) *Sxo);
       }
 
     	cudaMemcpy(output, d_out, sizeof(float)*Sxo, cudaMemcpyDeviceToHost);
-	
+	//free
     	cudaFree(d_Weight);
     	cudaFree(d_input);
     	cudaFree(d_out);
