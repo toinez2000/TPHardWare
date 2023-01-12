6736a53c1dc0403a4588e3244042f23691305bae
Antoine ZUNDEL <92809568+toinez2000@users.noreply.github.com>
2023-01-12 19:11:10 +0100

    Update meanPoolingTanH.h

--- meanPoolingTanH.h
+++ meanPoolingTanH.h
@@ -1,7 +1,21 @@
+
+/**
+* function activation_tanh appli tanH to output layer
+**/
 __device__ float activation_tanh(float M);
+
+
+/**
+* function meanPooling with GPU
+*return output matrix  size SxI*SxI*SzI/4
+**/
 
 
 __global__ void meanPooling(float* input, float* output, int SxI, int SzI);
 
+
+/**
+* function vectorGPUMeanPooling manage Layer meanPooling
+**/
 
 float* vectorGPUMeanPooling(float*input,int SxI,int SzI);
