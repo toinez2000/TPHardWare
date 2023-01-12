e06a5358b63e541b301c389fa71b298630ae3056
Antoine ZUNDEL <92809568+toinez2000@users.noreply.github.com>
2023-01-12 18:52:45 +0100

    Update functionMatrix.h

--- functionMatrix.h
+++ functionMatrix.h
@@ -1,7 +1,30 @@
+
+
+/**
+*function print float matrix sizeX*sizeY
+**/
+
 void print_matrix(float* matrice,int sizeX,int sizeY);
 
+/**
+*function init_matrix return a float matrix sizeX*sizeY init random value between (-1,1)
+**/
 float* init_matrix(int sizeX, int sizeY);
 
+/**
+*function addMatrix return a float matrix sizeX*sizeY add matrice A and B use CPU
+*param matriceA
+*param matriceB
+*return float matrix sizeX*sizeY
+**/
+
 float* addMatrix(float* matriceA , float* matriceB, int sizeX, int sizeY);
+
+/**
+*function multiMatrix return a float matrix multi matrice A and B use CPU
+*param matriceA
+*param matriceB
+*return float matrix 
+**/
 
 float* multiMatrix(float* matriceA , float* matriceB, int sizeXa, int sizeYa,int sizeXb,int sizeYb);
